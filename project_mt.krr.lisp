c7040b2076dd04ce0a5923ba65b3ca26f2565210
Chloe <chloebrown2020@u.northwestern.edu>
2020-03-03 16:12:23 -0600

    first file

---
+++ project_mt.krr.lisp
@@ -0,0 +1,2 @@
+; things to code: dndClass(classname), dndSubclass(subclassname, classname), dndRace(racename), dndStat(statname), dndSkill(skillname), 
+; dndSkillStat(skillname, statname), dndClassSave(statname, classname)
